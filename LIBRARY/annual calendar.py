81b00adb786705a4b5830646e1dbf4dc6590ad4c
Vanshika <vanshikag0111@gmail.com>
2021-05-26 19:10:47 +0530

    annual and monthly calendar

---
+++ LIBRARY/annual calendar.py
@@ -0,0 +1,11 @@
+#to print the calendar of particular month and year
+import calendar
+year = 2021
+month = 5
+print(calendar.month(year,month))
+
+#to print the calendar of particular year
+import calendar 
+print ("The calender of year 2021 is : ") 
+print (calendar.calendar(2021, 2, 1, 6)) 
+
