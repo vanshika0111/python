e6c3e2dd7313cec8a3ecaa177f95ad76024aead6
Vanshika <vanshikag0111@gmail.com>
2021-06-04 23:05:59 +0530

    concept of obj and class

--- OOP/class and object.py
+++ OOP/class and object.py
@@ -1,16 +1,19 @@
-class: it is a template in which all the 
+"""
+class: It is a template in which all the 
 format is already typed you just need to enter 
 the required details.
 
-object: it is the instance of class. 
+object: It is the instance of class. 
 A template is filled with
 all the details and now it is called object.
 
-example: bank form is a class 
+example: Bank form is a class 
 while the same filled form is called object.
 
 Note that, object is created from class.
 
 OOP follows the conecpt of DRY
 that is Do not Repeat Yourself.
-there's no need to re-write the snippet.
+There's no need to re-write the same snippet again.
+
+"""
