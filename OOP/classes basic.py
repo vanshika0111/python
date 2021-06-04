80f2c55861743a27faf70acec127a3a975ce6c26
Vanshika <vanshikag0111@gmail.com>
2021-06-04 16:22:02 +0530

    basic concept of class

---
+++ OOP/classes basic.py
@@ -0,0 +1,19 @@
+class student:
+    
+	pass
+
+
+
+abc = student()
+
+xyz = student()
+
+
+
+#abc and xyz are not assigned to student()
+#they both are two different objcets which is stored in the class student
+
+print(abc, xyz)
+
+#thus the print statement gives the info about the memory allocations of both objects with two different addresses
+
