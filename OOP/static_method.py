c903ea2f2df28649872722588058d423dd9f2848
Vanshika <vanshikag0111@gmail.com>
2021-08-29 10:48:03 +0530

    static method

---
+++ OOP/static_method.py
@@ -0,0 +1,10 @@
+# static method
+
+class Employee:
+    def display(string):
+        print("This is a " + string)
+        return "night"
+
+inked = Employee()
+inked.display("day")
+Employee.display("evening")
