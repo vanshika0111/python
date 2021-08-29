d26ab9e0da8b48e0869b17d48aa28d8b6461dec6
Vanshika <vanshikag0111@gmail.com>
2021-08-29 19:32:46 +0530

    short-hand of if-else

---
+++ CONCEPTS/short hand if-else.py
@@ -0,0 +1,7 @@
+# if-else can be short handed
+
+a = int(input("Enter A's value: "))
+b = int(input("Enter B's value: "))
+
+# if a>b: print("A is greater than B")
+print("B is greater than A") if a<b else print("A is greater than B")
