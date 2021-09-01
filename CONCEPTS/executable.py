9beae46b4c1f22cf3b1bbf8fac2f89ee113f0f42
Vanshika <vanshikag0111@gmail.com>
2021-09-01 16:56:28 +0530

    .py or .txt --> .exe

---
+++ CONCEPTS/executable.py
@@ -0,0 +1,29 @@
+# to generate .py to .exe file
+
+"""
+This is done with a module pyinstaller
+It is not a built-in module
+Command: pip install pyinstaller
+"""
+
+"""
+STEPS:
+    1. install pyinstaller
+    2. create a file or program like main.txt or main.py
+    3. run command (in terminal): python main.txt + tab
+    4. the program will be executed
+    5. run command in terminal: pyinstaller main.txt
+    6. build and dist directories will be created
+    7. explore dist dir, search for .exe file
+    8. now you have the .exe file
+    9. run command in terminal: pyinstaller --onefile main.txt
+        to et just the exe file & not the other stuff
+"""
+"""
+this file includes: 1. main.txt
+                    2. build dir
+                    3. dist dir
+                    4. main,txt.spec
+                    5. main.spec
+                    6. main.txt.txt
+"""
