f604a50bce55ce9d7e6ce5dc7144c81a92d1e63d
Vanshika <vanshikag0111@gmail.com>
2021-09-02 22:43:13 +0530

    file carrying the access of user-made module

---
+++ LIBRARY/Packages/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup #, find_packages
+setup(name="PackageOne",
+varsion = "0.1",
+description = "this is the first version of this midule",
+long_description = "this is a long description",
+author = "Vanshika",
+packages = ['PackageOne'],
+install_requirement = [] )
