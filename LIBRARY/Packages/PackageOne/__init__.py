491e8051774c47c7daacfd0cd1733f437222ec9d
Vanshika <vanshikag0111@gmail.com>
2021-09-02 22:42:14 +0530

    PackageOne dir carrying the user-made module

---
+++ LIBRARY/Packages/PackageOne/__init__.py
@@ -0,0 +1,8 @@
+def func(number):
+    print("This is a function")
+    return number
+
+# create class or def or anything which can be imported
+# udpate the setup.py accoridng to version and description
+# run the commands in the terminal & windows power shell
+
