39f33fa810df96053cb72a0a482c892cb572fbea
Vanshika <vanshikag0111@gmail.com>
2021-09-12 16:34:10 +0530

    age counter

---
+++ OUTPUT/age_counter.py
@@ -0,0 +1,27 @@
+YearAge = int (input("Enter you age or birth year: "))
+
+isAge = False
+isYear = False
+
+if len(str(YearAge)) == 4:
+    isYear = True
+else:
+    isAge = True
+# else:
+#     print("There was some problem.")
+#     exit()
+
+if (YearAge < 1900 and isYear):
+    print("Wow! You seem to be one of the oldest person alive.")
+    exit()
+if YearAge > 2021:
+    print("You sure you are born yet?")
+    exit()
+
+if isAge:
+    YearAge = 2021 - YearAge
+
+print(f"You will be of 100 years in {YearAge + 100}")
+
+Year = int(input("Enter the year you want to know age in: "))
+print(f"You will be {Year-YearAge} years old in {Year}")
