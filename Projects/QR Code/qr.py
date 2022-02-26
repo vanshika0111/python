5a2aab0f61b011c048073c6c90b88b6905196a49
Vanshika <vanshikag0111@gmail.com>
2022-02-26 12:22:14 +0530

    qr code

--- Projects/QR Code/qr.py
+++ Projects/QR Code/qr.py
@@ -4,6 +4,13 @@ import qrcode
 
 data = 'This QR code is created by Vanshika'
 
-image = qrcode.make(data)
+# -------------------------------- Simple QR Code ---------------------------------------
+# image = qrcode.make(data)
+# image.save('C:/Users/VANSHIKA/Desktop/coding_world/python/Projects/QR Code/myqr.png')
 
+# --------------------------------- Format QR Code ---------------------------------------
+qr = qrcode.QRCode(version = 1, box_size= 10, border= 5)
+qr.add_data(data)
+qr.make(fit=True)
+image = qr.make_image(fill_color = 'blue', back_color = 'white')
 image.save('C:/Users/VANSHIKA/Desktop/coding_world/python/Projects/QR Code/myqr.png')
