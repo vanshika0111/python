75f90417eea5b71f8d15f88359b84e60f6ec90b1
Vanshika <vanshikag0111@gmail.com>
2022-02-26 11:58:21 +0530

    configured py path for pycharm

---
+++ OUTPUT/tetris.py
@@ -0,0 +1,96 @@
+import turtle
+
+window = turtle.Screen()
+window.title("Tetris")
+window.bgcolor("black")
+window.setup(width=800, height=600)
+window.tracer(0)
+
+# paddle A
+paddleA = turtle.Turtle()
+paddleA.speed(0)
+paddleA.shape("square")
+paddleA.color("white")
+paddleA.shapesize(stretch_len=1, stretch_wid=5)
+paddleA.penup()
+paddleA.goto(-350,0)
+
+# paddle B
+paddleB = turtle.Turtle()
+paddleB.speed(0)
+paddleB.shape("square")
+paddleB.color("white")
+paddleB.shapesize(stretch_len=1, stretch_wid=5)
+paddleB.penup()
+paddleB.goto(350,0)
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0,0)
+ball.dx = 2
+ball.dy = -2 
+
+# Functions
+def paddleAUp():
+    y = paddleA.ycor()
+    y += 20
+    paddleA.sety(y)
+
+def paddleADown():
+    y = paddleA.ycor()
+    y -= 20
+    paddleA.sety(y)
+
+def paddleBUp():
+    y = paddleB.ycor()
+    y += 20
+    paddleB.sety(y)
+
+def paddleBDown():
+    y = paddleB.ycor()
+    y -= 20
+    paddleB.sety(y)
+
+# keyboard binding
+window.listen()
+window.onkeypress(paddleAUp, "w")
+window.onkeypress(paddleADown, "s")
+window.onkeypress(paddleBUp, "Up")
+window.onkeypress(paddleBDown, "Down")
+
+while True:
+    window.update()
+
+    # move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # border
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    if ball.xcor() > 390:
+        ball.goto(0,0)
+        ball.dx *= -1
+
+    if ball.xcor() < -390:
+        ball.goto(0,0)
+        ball.dx *= -1
+
+    # ball & paddle collisions
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddleB.ycor() + 40 and ball.ycor() > paddleB.ycor() - 40):
+        ball.setx(340)
+        ball.dx *= -1
+
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddleB.ycor() + 40 and ball.ycor() > paddleB.ycor() - 40):
+        ball.setx(-340)
+        ball.dx *= -1
